7e349bfbb3f7838494ceed2217e7ad46b6816f75
Gustave Monod <gustave.monod@insa-lyon.fr>
2013-12-18 14:40:38 +0100

    All files are now loaded only once.

--- ResourceManager.py
+++ ResourceManager.py
@@ -4,9 +4,9 @@ ResourceManager gets the images and loads them in memory whenever they are
 accessed via getFrame.
 """
 
-_resources = { }
-_paths = { }
-BATCH_SIZE = 5
+_resources = { } # { '/fh/path' : [b'', b'', b''] }
+_paths = { } # { 1 : ['/fh/path1', '/fh/path2'] }
+BATCH_SIZE = 2
 
 def addResource(mediaId, framePaths):
 	"""
@@ -20,12 +20,10 @@ def getFrame(mediaId, frameId):
 	Returns serialized image frameId of the media mediaId and id of the next available frame id.
 	frameId is an integer representing the offset in the frame list.
 	"""
-	if mediaId not in _resources or frameId not in _resources.get(mediaId, []):
-		_loadFramesBatch(mediaId, int(frameId / BATCH_SIZE))
-		print("media {} (frames {} to {}) is now in cache."
-			.format(mediaId, frameId, frameId + BATCH_SIZE))
+	if _paths[mediaId][frameId] not in _resources:
+		_loadFrameBatch(mediaId, int(frameId / BATCH_SIZE))
 
-	return (_resources[mediaId][frameId], getNextFrameId(mediaId, frameId))
+	return (_resources[_paths[mediaId][frameId]], getNextFrameId(mediaId, frameId))
 
 def getNextFrameId(mediaId, frameId):
 	"""
@@ -37,19 +35,16 @@ def getNextFrameId(mediaId, frameId):
 	else:
 		return 0
 
-def _loadFramesBatch(mediaId, batchNumber):
+def _loadFrameBatch(mediaId, batchNumber):
 	"""
 	Loads all frames for the media mediaId (into _resources),
 	BATCH_SIZE by BATCH_SIZE (ex: 5 by 5).
 	"""
-	if mediaId not in _resources:
-			_resources[mediaId] = {}
 
 	# TODO : handle resource not found
 	index = batchNumber * BATCH_SIZE
-	i = index
 	for path in _paths[mediaId][index:index + BATCH_SIZE]:
-		imageFile = open(path, 'rb')
-		# We load our images as bytestreams
-		_resources[mediaId][i] = bytes(imageFile.read())
-		i += 1
+		if path not in _resources:
+			imageFile = open(path, 'rb')
+			# We load our images as bytestreams
+			_resources[path] = bytes(imageFile.read())
