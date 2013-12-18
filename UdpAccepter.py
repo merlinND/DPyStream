93072f85bc3946bd32429bb58e6bdfd251457df8
Gustave Monod <gustave.monod@insa-lyon.fr>
2013-12-18 10:42:11 +0100

    Changed call of the UdpSocket constructor.

--- UdpAccepter.py
+++ UdpAccepter.py
@@ -3,14 +3,14 @@ from UdpSocket import *
 
 class UdpAccepter:
 	"""
-	UdpAccepter makes it possible to know when to create
-	a new UdpSocket for a new client.
+	UdpAccepter makes it possible to know when to create a new UdpSocket for a new client.
+	It receives messages from all clients and redirects them to the appropriate UdpSocket.
+	The UdpSocket adds the message to its buffer which is then read by its parent UdpHandler.
 	"""
 
 	def __init__(self, receiveBuffer = 4096, s = None):
 		if s is None:
-			self.commonSocket = socket.socket(socket.AF_INET,\
-											  socket.SOCK_DGRAM)
+			self.commonSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		else:
 			self.commonSocket = s
 		self.receiveBuffer = receiveBuffer
@@ -27,17 +27,16 @@ class UdpAccepter:
 		host = None
 		port = None
 
-		message, (host, port) = self.commonSocket.recvfrom	\
-								  (self.receiveBuffer)
+		message, (host, port) = self.commonSocket.recvfrom(self.receiveBuffer)
 
 		client = host + ":" + str(port)
 
-		# Adding client socket to the list
+		# If necessary, create a new UdpSocket instance for this client and add it to the list
 		if client not in self._connectedClients:
 			newClient = True
-			self._connectedClients[client] = \
-					UdpSocket(self.commonSocket, client)
+			self._connectedClients[client] = UdpSocket(self.commonSocket, host, port)
 
+		# Forwards the received to the UdpSocket corresponding to this client
 		self._connectedClients[client].receive(message)
 		
 		if newClient:
